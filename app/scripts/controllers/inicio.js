62f6a866a2dcd303b503231c209f9907b275ca10
monica90 <monicacentelles@gmail.com>
2016-01-25 19:04:38 +0100

    miweb

---
+++ app/scripts/controllers/inicio.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name miwebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the miwebApp
+ */
+angular.module('miwebApp')
+  .controller('InicioCtrl', function () {
+
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
